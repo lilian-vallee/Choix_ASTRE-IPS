b24bbc4cc29d33485930395ff1f1d177b155c4f5
Lilian.V <lilian.vallee1108@gmail.com>
2020-12-11 15:10:48 +0100

    saveModel() créé

--- src/main/java/com/ensim/choixAstreIPS/Moteur.java
+++ src/main/java/com/ensim/choixAstreIPS/Moteur.java
@@ -3,10 +3,28 @@ package com.ensim.choixAstreIPS;
 
 import com.ensim.choixAstreIPS.Model.Etudiant;
 import com.ensim.choixAstreIPS.Model.QuestionModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Moteur {
     private List<QuestionModel> models;
     private List<Etudiant> etudiants;
+
+    public Boolean saveModel(){
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File("src/main/resources/static/Save/Model_Save.json"), this.models);
+
+            return true;
+        } catch (IOException e) {
+            Logger.getAnonymousLogger().log(Level.WARNING,"Echec à l'ecriture de l'objet en String");
+            return false;
+        }
+    }
 }
