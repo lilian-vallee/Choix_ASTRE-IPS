8b07102a9da163a8bdfea51ded5c91b439574b69
Lilian.V <lilian.vallee1108@gmail.com>
2020-12-11 14:14:46 +0100

    prmière classe du model

---
+++ src/main/java/com/ensim/choixAstreIPS/Moteur.java
@@ -0,0 +1,12 @@
+package com.ensim.choixAstreIPS;
+
+
+import com.ensim.choixAstreIPS.Model.Etudiant;
+import com.ensim.choixAstreIPS.Model.QuestionModel;
+
+import java.util.List;
+
+public class Moteur {
+    private List<QuestionModel> models;
+    private List<Etudiant> etudiants;
+}
