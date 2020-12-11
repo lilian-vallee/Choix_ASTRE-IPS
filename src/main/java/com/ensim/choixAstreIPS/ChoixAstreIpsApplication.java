0aef603b179cd0b61181a750f5ab77a57aa9690d
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-12-11 14:03:47 +0100

    Initial project

---
+++ src/main/java/com/ensim/choixAstreIPS/ChoixAstreIpsApplication.java
@@ -0,0 +1,13 @@
+package com.ensim.choixAstreIPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoixAstreIpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoixAstreIpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensim/choixAstreIPS/ChoixAstreIpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensim.choixAstreIPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoixAstreIpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
