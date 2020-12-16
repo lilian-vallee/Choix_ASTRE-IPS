32f60fa082973f56ad74eee817a9b6f37f04caeb
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-12-16 14:39:39 +0100

    Stylized etudiants list form + jQuery added

--- src/main/java/com/ensim/choixAstreIPS/Controller/EtudiantController.java
+++ src/main/java/com/ensim/choixAstreIPS/Controller/EtudiantController.java
@@ -1,19 +1,29 @@
 package com.ensim.choixAstreIPS.Controller;
 
+import com.ensim.choixAstreIPS.Model.Etudiant;
 import com.ensim.choixAstreIPS.Moteur;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EtudiantController {
 
+    private final Moteur moteur = new Moteur();
+
     @GetMapping("/etudiants")
     public String getEtudiants(Model model) {
-        Moteur moteur = new Moteur();
-        moteur.loadQuestionnaire3A();
+        if( moteur.getEtudiants().size() == 0 )
+            moteur.loadQuestionnaire3A();
         model.addAttribute("allEtudiants", moteur.getEtudiants());
         return "etudiants";
+    }
+
+    @GetMapping("/reponses")
+    public String getReponsesFromEtudiant(@RequestParam String etu, Model model) {
+        Etudiant etud = moteur.getEtudiants().stream().filter(etudiant -> etu.equals(etudiant.getId())).findFirst().orElse(new Etudiant());
+        model.addAttribute("etudiant", etud);
+        return "reponses";
     }
 }
