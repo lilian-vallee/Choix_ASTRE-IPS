8e92e51972fec4aef0d64d245fd85383fa15a119
Lilian.V <lilian.vallee1108@gmail.com>
2020-12-17 14:49:55 +0100

    Edittion des model de question + lien ver l'édition des mots clés

--- src/main/java/com/ensim/choixAstreIPS/Controller/QuestionModelEditController.java
+++ src/main/java/com/ensim/choixAstreIPS/Controller/QuestionModelEditController.java
@@ -1,5 +1,6 @@
 package com.ensim.choixAstreIPS.Controller;
 
+import com.ensim.choixAstreIPS.Model.MotCle;
 import com.ensim.choixAstreIPS.Model.QuestionModel;
 import com.ensim.choixAstreIPS.Moteur;
 import org.springframework.stereotype.Controller;
@@ -13,12 +14,14 @@ public class QuestionModelEditController {
     public String getModels(@RequestParam int index, Model model) {
         QuestionModel questionModel = Moteur.INSTANCE.getModels().get(index-1);
         model.addAttribute("questionModel", questionModel);
-        model.addAttribute("getMotCle", questionModel.getMotsCles());
+        model.addAttribute("getMotsCles", questionModel.getMotsCles());
+        model.addAttribute("newMotCle", new MotCle("mot",0,0));
         return "QuestionModelEdit";
     }
 
-    @PostMapping("/SaveCoeff")
-    public void SaveCoeff(@ModelAttribute QuestionModel questionModel){
-
+    @PostMapping("/QuestionModelEdit")
+    public String SaveCoeff(@ModelAttribute QuestionModel questionModel, @RequestParam int index){
+        Moteur.INSTANCE.getModels().set(index-1, questionModel);
+        return "QuestionModelEdit";
     }
 }
