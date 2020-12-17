ee579026e7102c31e3247f2b4f6e4dd460f3eaf7
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-12-17 14:09:14 +0100

    Cleaned up some code

--- src/main/java/com/ensim/choixAstreIPS/Controller/EtudiantController.java
+++ src/main/java/com/ensim/choixAstreIPS/Controller/EtudiantController.java
@@ -3,7 +3,6 @@ package com.ensim.choixAstreIPS.Controller;
 import com.ensim.choixAstreIPS.Model.Etudiant;
 import com.ensim.choixAstreIPS.Model.Repository.EtudiantRepository;
 import com.ensim.choixAstreIPS.Moteur;
-import com.ensim.choixAstreIPS.Utils.QuestionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +15,10 @@ public class EtudiantController {
     @Autowired
     EtudiantRepository etudiantRepository;
 
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
 
     @GetMapping("/etudiants")
     public String getEtudiants(@RequestParam String etu, Model model) {
@@ -31,15 +34,5 @@ public class EtudiantController {
         model.addAttribute("etudiant", etud );
 
         return "etudiants";
-    }
-
-    @GetMapping("/reponses")
-    public String getReponsesFromEtudiant(@RequestParam String etu, Model model) {
-        Etudiant etud = new Etudiant();
-        if(etudiantRepository.findById(etu).isPresent() ) {
-            etud = etudiantRepository.findById(etu).get();
-        }
-        model.addAttribute("etudiant", etud );
-        return "reponses";
     }
 }
