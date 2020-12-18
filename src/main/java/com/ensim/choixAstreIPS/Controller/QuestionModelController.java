533fbf112327b15ceef7f0bb8fbd913b9b1a07fb
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-12-18 16:41:50 +0100

    Add a new QuestionModel

--- src/main/java/com/ensim/choixAstreIPS/Controller/QuestionModelController.java
+++ src/main/java/com/ensim/choixAstreIPS/Controller/QuestionModelController.java
@@ -1,15 +1,31 @@
 package com.ensim.choixAstreIPS.Controller;
 
+import com.ensim.choixAstreIPS.Model.QuestionModel;
 import com.ensim.choixAstreIPS.Moteur;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class QuestionModelController {
 
     @GetMapping("/QuestionModel")
     public String getModels(Model model) {
+        model.addAttribute("allQuestionsModels", Moteur.INSTANCE.getModels());
+        return "QuestionModel";
+    }
+
+    @GetMapping("/QuestionModelAdd")
+    public String addQuestionModel(Model model) {
+        model.addAttribute("qm_index", Moteur.INSTANCE.getModels().size() + 1);
+        return "QuestionModelAdd";
+    }
+
+    @PostMapping("/QuestionModel")
+    public String addedQuestionModel(@RequestParam int index, @RequestParam String intitule, @RequestParam double coeff, Model model) {
+        Moteur.INSTANCE.addModel(new QuestionModel(index, intitule, coeff));
         model.addAttribute("allQuestionsModels", Moteur.INSTANCE.getModels());
         return "QuestionModel";
     }
